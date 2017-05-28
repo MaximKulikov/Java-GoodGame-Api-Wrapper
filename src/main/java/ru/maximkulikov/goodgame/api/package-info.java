fd20e100b02f2a0a539efa1fd7419a14e2b8d3f7
Maxim Kulikov <maximkulikov@live.ru>
2017-05-29 01:01:09 +0300

    Описана документация javadoc

--- src/main/java/ru/maximkulikov/goodgame/api/package-info.java
+++ src/main/java/ru/maximkulikov/goodgame/api/package-info.java
@@ -1,5 +1,11 @@
 /**
- * Java адапер для работы с Goodgame (GoodGame - API стримингового сервиса и GoodChat - API чата)
+ * Java GoodGame Api Wrapper (JGAW) - обертка над API GoodGame , позволяющая выполнять асинхронные запросы
+ * к стриминговому сервису, а так же работать с чатом через websocket.
+ * <br><br>
+ * За основу проекта взята разработка `Matthew J. Bell @urgrue` Java-Twitch-Api-Wrapper https://github.com/urgrue/Java-Twitch-Api-Wrapper
+ * <br><br>
+ * @autor Maxim Kulikov
+ *
  */
 
 package ru.maximkulikov.goodgame.api;
